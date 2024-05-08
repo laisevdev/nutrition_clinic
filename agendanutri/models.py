f71dac212cd22b2e4aa17db75b4bda3a6869a1d3
Laíse Vieira <laisevdev@gmail.com>
2024-05-08 16:09:28 -0300

    [feat]: add Agenda Model

--- agendanutri/models.py
+++ agendanutri/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Paciente(models.Model):
-    _id = models.CharField(primary_key=True, max_length=14)
     nome = models.CharField(max_length=100)
     cpf = models.CharField(max_length=14, unique=True)
     endereco = models.CharField(max_length=200)
@@ -15,7 +15,6 @@ class Paciente(models.Model):
     idade = models.PositiveIntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)  
     altura = models.DecimalField(max_digits=3, decimal_places=2)  
-    data_consulta = models.DateTimeField(auto_now = False, auto_now_add = False, default=timezone.now())
 
     def __str__(self):
         return self.nome
@@ -25,7 +24,7 @@ class Agenda(models.Model):
     data_retorno = models.DateTimeField(auto_now = False, auto_now_add = False, default=timezone.now())
     data_reagendamento = models.DateTimeField(auto_now = False, auto_now_add = False, default=timezone.now())
     data_criacao = models.DateTimeField(auto_now = False, auto_now_add = False, default=timezone.now())
-    id_paciente = models.ForeignKey(Paciente, on_delete=models.SET_NULL)
+    id_paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
 
     def criacao(self):
         self.data_criacao = timezone.now()
