83587e8bded3e674d34a846ad2b4cb58dcf81373
Laíse Vieira <laisevdev@gmail.com>
2024-05-10 00:30:50 -0300

    [feat] add def marcar_consulta view

--- agendanutri/views.py
+++ agendanutri/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'agendanutri/index.html', {})
 
+def marcar_consulta(request):
+    return render (request, 'agendanutri/marcar_consulta.html', {})
