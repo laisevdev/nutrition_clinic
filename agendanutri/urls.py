159eb7fd319307f392303c6c193987c0e0eeeafd
Laíse Vieira <laisevdev@gmail.com>
2024-05-10 23:04:26 -0300

    [feat]: add agendamentos path

--- agendanutri/urls.py
+++ agendanutri/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('agendanutri/marcar', views.marcar_consulta, name='marcar_consulta'),
+    path('agendanutri/marcar/', views.marcar_consulta, name='marcar_consulta'),
+    path('agendanutri/agendamentos/', views.lista_consultas, name='lista_consultas'),
 ]
