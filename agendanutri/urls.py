549da297cd7d713d6f30793208400d9f4a798df5
Laíse Vieira <laisevdev@gmail.com>
2024-05-11 14:23:32 -0300

    [feat]: add cadastrar url

--- agendanutri/urls.py
+++ agendanutri/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('agendanutri/cadastrar/', views.cadastrar, name='cadastro_pacientes'),
     path('agendanutri/marcar/', views.marcar_consulta, name='marcar_consulta'),
     path('agendanutri/agendamentos/', views.lista_consultas, name='lista_consultas'),
 ]
