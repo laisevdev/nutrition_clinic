1699ddfebb40af93c0cdb2f395f9fdc4546d1f11
Laíse Vieira <laisevdev@gmail.com>
2024-05-11 23:51:23 -0300

    [feat]: add listadepacientes url path

--- agendanutri/urls.py
+++ agendanutri/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('agendanutri/cadastrar/', views.cadastrar, name='cadastro_pacientes'),
     path('agendanutri/cadastradocomsucesso/', views.sucesso_cadastro, name='sucesso_cadastro'),
+    path('agendanutri/listadepacientes/', views.exibir_lista_pacientes, name='lista_pacientes'),
     path('agendanutri/marcar/', views.marcar_consulta, name='marcar_consulta'),
     path('agendanutri/agendamentos/', views.lista_consultas, name='lista_consultas'),
 ]
