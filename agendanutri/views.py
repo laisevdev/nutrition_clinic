7c73a37373cd28c819c707f1cdcc95d9ba0f8155
Laíse Vieira <laisevdev@gmail.com>
2024-05-13 20:56:05 -0300

    [feat]: add def deletar_paciente view

--- agendanutri/views.py
+++ agendanutri/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AgendaForm, PacienteForm
-from .models import Agenda
-from .models import Paciente
-from django.utils import timezone
+from .models import Agenda, Paciente
+
 
 def index(request):
     return render(request, 'agendanutri/index.html', {})
@@ -42,6 +41,14 @@ def editar_pacientes(request, pk):
     else:
         form = PacienteForm(instance=paciente)
     return render(request, 'agendanutri/editar_pacientes.html', {'form': form}) 
+
+def deletar_pacientes(request, pk):
+    paciente = get_object_or_404(Paciente, pk=pk)
+    if request.method == "POST":
+        paciente.delete()
+        return redirect('paciente_deletado', pk=pk)
+    else:
+        return render(request, 'agendanutri/paciente_deletado.html', {}) 
 
 def marcar_consulta(request):        
     if request.method == "POST":
