6f42402185cb29a45cc20c4f3353a9c6342f1335
Laíse Vieira <laisevdev@gmail.com>
2024-05-13 20:58:24 -0300

    [feat]: add deletarpaciente url path

--- agendanutri/urls.py
+++ agendanutri/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('agendanutri/listadepacientes/', views.exibir_lista_pacientes, name='lista_pacientes'),
     path('agendanutri/dadospaciente/<int:pk>/', views.dados_paciente, name='dados_paciente'),
     path('agendanutri/<int:pk>/editarpaciente/', views.editar_pacientes, name='editar_pacientes'),
+    path('agendanutri/deletarpaciente/<int:pk>/', views.deletar_pacientes, name='paciente_deletado'),
     path('agendanutri/marcar/', views.marcar_consulta, name='marcar_consulta'),
     path('agendanutri/agendamentos/', views.lista_consultas, name='lista_consultas'),
 ]
