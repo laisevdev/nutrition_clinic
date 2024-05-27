052d65a56d0d1fcb1e9cc900d02476c84cbcddfb
Laíse Vieira <laisevdev@gmail.com>
2024-05-27 16:27:01 -0300

    feat: add hours choices

--- agendanutri/forms.py
+++ agendanutri/forms.py
@@ -4,11 +4,24 @@ from .models import Agenda, Paciente
 
 class AgendaForm(forms.ModelForm):
 
+    HORARIOS_DISPONIVEIS = [
+        ("08:00", "08:00"),
+        ("09:00", "09:00"),
+        ("10:00", "10:00"),
+        ("13:00", "13:00"),
+        ("14:00", "14:00"),
+        ("15:00", "15:00"),
+        ("16:00", "16:00"),
+        ("17:00", "17:00"),
+    ]
+    
+    horario_consulta = forms.ChoiceField(choices=HORARIOS_DISPONIVEIS, label="Horário")
+
     class Meta:
         model = Agenda
         fields = ('data_consulta', 'id_paciente',)
         widgets = {
-            'data_consulta': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'data_consulta': forms.DateInput(attrs={'type': 'date'}),
         }
 
 class PacienteForm(forms.ModelForm):
